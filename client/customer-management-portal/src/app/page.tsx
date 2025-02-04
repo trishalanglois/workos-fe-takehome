326f72552d5511f3b06a9fb1c0529cc2993c378c
Trisha Langlois <tlanglois@gofundme.com>
2025-02-03 18:48:08 -0800

    Add search and button components

--- client/customer-management-portal/src/app/page.tsx
+++ client/customer-management-portal/src/app/page.tsx
@@ -1,8 +1,21 @@
 import '@radix-ui/themes/styles.css';
 
-import { Box, Container, Tabs, Theme } from '@radix-ui/themes';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Tabs,
+  TextField,
+  Theme,
+} from '@radix-ui/themes';
 import Users from './Users';
 import Roles from './Roles';
+import {
+  BookmarkIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+} from '@radix-ui/react-icons';
 
 const PageWrapper = ({ children }: { children: React.ReactNode }) => (
   <Box display={'block'} py={'var(--space-7)'} px={'2'}>
@@ -10,30 +23,69 @@ const PageWrapper = ({ children }: { children: React.ReactNode }) => (
   </Box>
 );
 
+const Search = () => {
+  return (
+    <TextField.Root
+      placeholder="Search by name…"
+      variant="classic"
+      style={{ width: '100%' }}
+    >
+      <TextField.Slot gap={'var(--space-2)'}>
+        <MagnifyingGlassIcon height="16" width="16" color="#000713" />
+      </TextField.Slot>
+    </TextField.Root>
+  );
+};
+
 export default function Home() {
   return (
     <Theme>
       <PageWrapper>
-        <Container size={'4'} style={{ border: '1px solid red' }}>
-          <Tabs.Root defaultValue="users">
-            <Tabs.List>
-              <Tabs.Trigger value="users">Users</Tabs.Trigger>
-              <Tabs.Trigger value="roles">Roles</Tabs.Trigger>
-            </Tabs.List>
-            <Box pt="3">
-              <Tabs.Content value="users">
-                <Users />
-              </Tabs.Content>
+        <Container>
+          <Flex
+            direction={'column'}
+            gap={'9'}
+            style={{ border: '1px solid red' }}
+          >
+            <Tabs.Root defaultValue="users">
+              <Tabs.List size={'1'}>
+                <Tabs.Trigger value="users">Users</Tabs.Trigger>
+                <Tabs.Trigger value="roles">Roles</Tabs.Trigger>
+              </Tabs.List>
 
-              <Tabs.Content value="roles">
-                <Roles />
-              </Tabs.Content>
-            </Box>
-          </Tabs.Root>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  gap: 'var(--space-2)',
+                  margin: 'var(--space-5) 0',
+                  border: '1px solid blue',
+                }}
+              >
+                <Search />
+
+                <Button color={'purple'}>
+                  <PlusIcon />
+                  Add user
+                </Button>
+              </div>
+
+              <Box>
+                <Tabs.Content value="users">
+                  <Users />
+                </Tabs.Content>
+
+                <Tabs.Content value="roles">
+                  <Roles />
+                </Tabs.Content>
+              </Box>
+            </Tabs.Root>
+          </Flex>
         </Container>
       </PageWrapper>
     </Theme>
   );
 }
 
-// TODO: update PageWrapper p to use the CSS variable instead of the string
+// TODO: extract Tabs.Content to Users and Roles components?
+// TODO: Figure out how to use the purple 9 accent color and add purple a3 for border
