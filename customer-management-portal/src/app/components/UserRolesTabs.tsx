c9e2f5a6fe67ed7cf0b6be404142730cd726dceb
Trisha Langlois <tlanglois@gofundme.com>
2025-02-08 16:25:08 -0800

    Update init and filtered data

--- customer-management-portal/src/app/components/UserRolesTabs.tsx
+++ customer-management-portal/src/app/components/UserRolesTabs.tsx
@@ -16,19 +16,18 @@ export default function UserRolesTabs() {
   const [loading, setLoading] = useState(true); // Track loading state
   const [requestError, setRequestError] = useState(false);
   const [filteredData, setFilteredData] = useState(tableData);
+  const [emptySearchResults, setEmptySearchResults] = useState(false);
 
   const handleSearch = (searchTerm: string) => {
     const filtered = tableData?.filter((item) =>
       Object.values(item)
-        .join(' ') 
+        .join(' ')
         .toLowerCase()
-        .includes(searchTerm.toLowerCase())
+        .includes(searchTerm.toLowerCase()),
     );
 
-    return setFilteredData(filtered);
+    return filtered ? setFilteredData(filtered) : setEmptySearchResults(true);
   };
-
-  // if no data matches, show empty state
 
   // fetch data for roles table
 
@@ -36,23 +35,26 @@ export default function UserRolesTabs() {
     const fetchTableData = async () => {
       try {
         const result = await fetchUsersWithRoles();
-  
+
+        console.log('TL init load -->', result)
+
         if (result) {
           const { usersWithRoles, hasError } = result;
-  
+
           setData(usersWithRoles);
-  
+          setFilteredData(usersWithRoles)
+
           if (hasError) {
             setRequestError(hasError);
           }
         }
       } catch (error) {
-        console.error("Error fetching table data:", error);
+        console.error('Error fetching table data:', error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
-  
+    };
+
     fetchTableData();
   }, []);
 
@@ -75,12 +77,12 @@ export default function UserRolesTabs() {
         </Tabs.Trigger>
       </Tabs.List>
       <Flex gap={'var(--space-2)'} justify={'between'} py={'var(--space-5)'}>
-        <Search onSearch={handleSearch}/>
+        <Search onSearch={handleSearch} />
         <AccentButton displayText="Add user" />
       </Flex>
 
       {requestError && (
-        <Callout.Root color="red" mb={"5"}>
+        <Callout.Root color="red" mb={'5'}>
           <Callout.Icon>
             <InfoCircledIcon />
           </Callout.Icon>
@@ -90,20 +92,28 @@ export default function UserRolesTabs() {
         </Callout.Root>
       )}
 
-      {loading ? (
-        <Flex justify={'center'}>
-          <Spinner size={'3'} loading={loading} />
-        </Flex>
-      ) : (
-        <Box>
-          <Tabs.Content value="users">
-            <DataTable dataType="users" tableData={filteredData} />
-          </Tabs.Content>
-          <Tabs.Content value="roles">
-            <DataTable dataType="roles" tableData={filteredData} />
-          </Tabs.Content>
-        </Box>
-      )}
+      <>
+        {loading && (
+          <Flex justify={'center'}>
+            <Spinner size={'3'} loading={loading} />
+          </Flex>
+        )}
+
+        {!loading && emptySearchResults && (
+          <p>No results matched your search.</p> // update to Text component
+        )}
+
+        {!loading && !emptySearchResults && (
+          <Box>
+            <Tabs.Content value="users">
+              <DataTable dataType="users" tableData={filteredData} />
+            </Tabs.Content>
+            <Tabs.Content value="roles">
+              <DataTable dataType="roles" tableData={filteredData} />
+            </Tabs.Content>
+          </Box>
+        )}
+      </>
     </Tabs.Root>
   );
 }
